904a8061949e4152de66f8f39256305ffbe54604
mahfuzur <mdmahfuazur7788@gamil.com>
2023-04-07 23:54:45 +0600

    variabls is js

---
+++ variables.js
@@ -0,0 +1,7 @@
+
+var name = "Md Mahfuzur Rahman";
+var age = 25;
+
+console.log(name + 'Knows JavaScript')
+console.log('His age is only ' + age)
+console.log(name + 'is crating a Javascript course For All');
