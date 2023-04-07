b4b56de05084d935cc13929315d756b99c6c846e
mahfuzur <mdmahfuazur7788@gamil.com>
2023-04-08 00:36:57 +0600

    Number for Js

--- variables.js
+++ variables.js
@@ -5,3 +5,27 @@ var age = 25;
 console.log(name + 'Knows JavaScript')
 console.log('His age is only ' + age)
 console.log(name + 'is crating a Javascript course For All');
+
+
+//JavaScript Data type are two primitive and object 
+//primitive -- Number , String , Boolean --> true and  false
+//undefine and NULL;
+
+//object are =--> Array, function , object
+
+//Number
+var a= 4;
+var b = 4.4
+var c = 4.555
+console.log(a)
+console.log(b)
+console.log(c)
+var a = Number(45.88)
+console.log(a)
+console.log(Number.parseFloat(a))
+console.log(Number.parseInt(a));
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+
+
+
