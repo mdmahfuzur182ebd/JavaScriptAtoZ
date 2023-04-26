313db28eb6e168ab003a9ac80d8c791c1b85ce45
mahfuzur <mdmahfuazur7788@gamil.com>
2023-04-26 20:07:15 +0600

    ternary Operation

--- hello.js
+++ hello.js
@@ -1,6 +1,21 @@
 
-console.log("hello world!")
-console.log(45)
-console.log(60.7787)
-console.log('mY fav no '+ 9)
-console.log( 9 +'9')
+// console.log("hello world!")
+// console.log(45)
+// console.log(60.7787)
+// console.log('mY fav no '+ 9)
+// console.log( 9 +'9')
+
+var n = 7
+var str =  ''
+
+if(n % 2 === 0){
+    str = 'EVEN'
+}else{
+    str= 'ODD'
+}
+console.log(str)
+
+var result = n % 2 === 0 ? 'Even': 'ODD'
+console.log(result);
+
+//condition ? true : false
