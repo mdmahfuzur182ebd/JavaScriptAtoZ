4286907625c122b3780fec44d4a45dbdd290884a
mahfuzur <mdmahfuazur7788@gamil.com>
2023-04-27 20:12:32 +0600

    while loop and nexted Looping

---
+++ whileloop.js
@@ -0,0 +1,33 @@
+
+var isRunning = true
+while(isRunning){
+    var rand = Math.floor(Math.random() * 10 + 1)
+    if(rand == 9){
+        console.log('Winner Winner Chicken Dinner')
+        isRunning = false
+    }else{
+        console.log('You have got '+ rand)
+    }
+}
+
+
+var n = 7;
+
+for(var i = 1 ; i<=n ; i++){
+    var result = " ";
+    for(var j=1; j<i; j++){
+        result += j + ''
+    }
+    console.log(result)
+}
+
+
+
+var n =7
+for (var i = 1; i<=n; i++){
+    var result = ''
+    for(var j=1; j<n; j++){
+         result += '*' + ''
+    }
+    console.log(result)
+}
