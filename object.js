5bc67901f6d3584d6ceb2bc7ed7ac3e7a0499d4b
mahfuzur <mdmahfuazur7788@gamil.com>
2023-04-29 14:49:18 +0600

    Accessing object Properties in JavaScript01~

--- object.js
+++ object.js
@@ -7,25 +7,40 @@
 // obj.y = 29
 // console.log(obj)
 
-var point = {  //literal
-    x: 10,
-    y: 20,
-    z: 30
+// var point = {  //literal
+//     x: 10,
+//     y: 20,
+//     z: 30
+// }
+// point.R = 89
+// point.T = 789
+// console.log(point)
+
+
+// var obj = Object()  //constructor
+// obj.x =19
+// obj.o = 45
+// console.log(obj)
+
+// var obj = new Object()  //object create new keyword
+// obj.A = 34
+// obj.B = 54
+// obj.C = 32
+
+// console.log(obj)
+
+//Accessing object Properties in JavaScript
+//Dot natation and [] array natation
+
+var point = {
+    x: 29,
+    name: "some",
+    age : 34,
+    year: '2023'
 }
-point.R = 89
-point.T = 789
-console.log(point)
 
-
-var obj = Object()  //constructor
-obj.x =19
-obj.o = 45
-console.log(obj)
-
-var obj = new Object()  //object create new keyword
-obj.A = 34
-obj.B = 54
-obj.C = 32
-
-console.log(obj)
-
+console.log(point.x)
+console.log(point.name)
+console.log(point.age)
+console.log(point['year'])
+console.log(point['x'])
