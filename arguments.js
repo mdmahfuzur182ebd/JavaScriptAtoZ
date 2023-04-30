8858b6935443b7b675839971839716b72fea10e6
mahfuzur <mdmahfuazur7788@gamil.com>
2023-04-30 21:23:59 +0600

    return function

--- arguments.js
+++ arguments.js
@@ -26,12 +26,26 @@ function sumOfSomething(){//wish your parameter
         sum+=arguments[i]
        // console.log(arguments[i])
     }
-    console.log(sum)
+    return sum
 }
 
-sumOfSomething(23,45,66,77) //unlimited arguments given
-sumOfSomething(1,2,3,4,5,6,7,8,9,10)//55
+var t = sumOfSomething(23,45,66,77) //unlimited arguments given
+var s = sumOfSomething(1,2,3,4,5,6,7,8,9,10)//55
 //sumOfSomething(11,12,13,14,15,16,17,18,19,20)//155
 //sumOfSomething(21,22,23,24,25,26,27,28,29,30)//255
 //sumOfSomething(31,32,33,34,35,36,37,38,39,40)//355
 
+console.log(s)
+console.log(t)
+
+
+function Person(fname, lname, age){
+    return {
+        firstName:fname,
+        lastName: lname,
+        age: age
+    }
+}
+
+let person = Person('Mahfuzur', 'Rahman', 23)
+console.log(person)
