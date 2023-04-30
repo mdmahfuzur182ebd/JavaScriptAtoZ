385384b3b9d12b29f5fa125d558251cac388a586
mahfuzur <mdmahfuazur7788@gamil.com>
2023-04-30 11:53:49 +0600

    object buliten function

--- object.js
+++ object.js
@@ -32,19 +32,68 @@
 //Accessing object Properties in JavaScript
 //Dot natation and [] array natation
 
-var point = {
-    x: 29,
-    name: "some",
-    age : 34,
-    year: '2023'
+// var point = {
+//     x: 29,
+//     name: "some",
+//     age : 34,
+//     year: '2023'
+// }
+
+// point.name = 'Nothing'
+// point['age'] = 23
+
+// console.log(point.x)
+// console.log(point.name)
+// console.log(point.age)
+// console.log(point['year'])
+// console.log(point['x'])
+// //console.log(point.d)
+// point.some = undefined
+// console.log(point)
+// delete point.some  //remove object Properties
+// console.log(point)
+
+//compression
+
+
+
+let obj1 = {
+    x:34,
+    some : 23,
+    age: 24,
+    name:"something"
 }
 
-point.name = 'Nothing'
-point['age'] = 23
+let obj2 = {
+    x:34,
+    some : 23,
+    age: 24,
+    name:"something"
+}
 
-console.log(point.x)
-console.log(point.name)
-console.log(point.age)
-console.log(point['year'])
-console.log(point['x'])
-//console.log(point.d)
+//console.log(obj1 === obj2)
+if(obj1.age == obj2.age && obj1.x == obj2.x){
+    console.log(true)
+}else{
+    console.log(false)
+}
+console.log(obj1)
+console.log(JSON.stringify(obj1))
+console.log(JSON.stringify(obj1) === JSON.stringify(obj2) )
+
+//Iteration of object
+
+let obj3 = {
+    id: 2,
+    name: "Nothing",
+    age: 34,
+    year : '2023',
+    country: ['Bangladesh', 'Nepal', 'India', 'Pakistani']
+}
+
+console.log('id' in obj3)
+
+for(var i in obj3){
+    console.log(i)
+    console.log(i +':'+obj3[i])
+}
